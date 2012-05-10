2bbb1c48e98e1bf96d96200bd046f763193754af
Harshad Sharma <harshad@harshadsharma.com>
2012-05-10 13:35:17 +0530

    Works for basic tweeting.

---
+++ quicktweet.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import tweepy
+
+try:
+    from credentials import *
+except:
+    CONSUMER_KEY = ''
+    CONSUMER_TOKEN = ''
+    ACCESS_KEY = ''
+    ACCESS_SECRET = ''
+
+def post_tweet(text):
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_TOKEN)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    api.update_status(text)
+
+if __name__ == '__main__':
+    import sys
+    import fileinput
+
+    if len(sys.argv) > 1:
+        text = ' '.join(sys.argv[1:])
+        print text
+        post_tweet(text)
+    else:
+        tweeted = False
+        for line in fileinput.input():
+            tweeted = True
+            post_tweet(line)
+
+        if tweeted == False:
+            print "Usage: %s [tweet]" % sys.argv[0]
